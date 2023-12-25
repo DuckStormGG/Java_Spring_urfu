54afc4d8681e021142b374299a67cd8b3aa35197
DuckStormGG <Ghost56YT@gmail.com>
2023-12-25 18:15:54 +0500

    mod 5 completed(1,2)

---
+++ skillBox/mod5/MyBookShopApp_m5_4/src/main/java/skbx/example/struct/book/file/BookFileEntity.java
@@ -0,0 +1,57 @@
+package skbx.example.struct.book.file;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Generated;
+import org.hibernate.annotations.GenerationTime;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "book_file")
+public class BookFileEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(columnDefinition = "VARCHAR(255) NOT NULL")
+    private String hash;
+    @Column(columnDefinition = "INT NOT NULL")
+    private int type_id;
+
+    @Column(columnDefinition = "VARCHAR(255) NOT NULL")
+    private String path;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public int getType_id() {
+        return type_id;
+    }
+
+    public void setType_id(int type_id) {
+        this.type_id = type_id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
